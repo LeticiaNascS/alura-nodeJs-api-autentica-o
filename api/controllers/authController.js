b8a6082b21d9e749d7d72f0a8885d236e0d58050
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-03-18 11:21:08 -0300

    Criando rota de login e token jwt

---
+++ api/controllers/authController.js
@@ -0,0 +1,19 @@
+const AuthService = require("../services/authServices.js");
+
+const authService = new AuthService();
+
+class AuthController {
+  static async login(req, res) {
+    const { email, senha } = req.body;
+
+    try {
+      const login = await authService.login({ email, senha });
+
+      res.status(200).send(login);
+    } catch (error) {
+      res.status(401).send(error.message);
+    }
+  }
+}
+
+module.exports = AuthController;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,8 @@
 const bodyParser = require("body-parser");
 
-const produto = require("./produtoRoute");
-const usuarios = require("./usuariosRoute");
+const produto = require("./produtoRoute.js");
+const usuarios = require("./usuariosRoute.js");
+const login = require("../routes/authRoutes.js");
 module.exports = (app) => {
-  app.use(bodyParser.json(), produto, usuarios);
+  app.use(bodyParser.json(), produto, usuarios, login);
 };
